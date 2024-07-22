49e2f4e4c0a82cbff4d488e457a411dcbe0baf34
Yun <unow1992@gmail.com>
2024-07-22 18:51:17 +0000

    norm inits.c

--- inits.c
+++ inits.c
@@ -6,21 +6,21 @@
 /*   By: yowoo <yowoo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 11:47:58 by yowoo             #+#    #+#             */
-/*   Updated: 2024/07/15 20:29:12 by yowoo            ###   ########.fr       */
+/*   Updated: 2024/07/22 18:42:10 by yowoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void init_struct(t_round *round, int argc, char **argv)
+void	init_struct(t_round *round, int argc, char **argv)
 {
-    round->n_philo = ft_atoi(argv[1]);
-    round->t_die = ft_atoi(argv[2]);
-    round->t_eat = ft_atoi(argv[3]);
-    round->t_sleep = ft_atoi(argv[4]);
-    if (argc == 6)
+	round->n_philo = ft_atoi(argv[1]);
+	round->t_die = ft_atoi(argv[2]);
+	round->t_eat = ft_atoi(argv[3]);
+	round->t_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
 	{
-        round->n_must_eat = ft_atoi(argv[5]);	
+		round->n_must_eat = ft_atoi(argv[5]);
 	}
 	else
 		round->n_must_eat = -1;
@@ -32,24 +32,16 @@ void init_struct(t_round *round, int argc, char **argv)
 	gettimeofday(&(round->current), NULL);
 }
 
-void init_philos(t_philo **philo, t_round *round)
+void	init_philos(t_philo **philo, t_round *round)
 {
-    int i;
+	int	i;
 
-    i = 0;
-	// *philo = (t_philo *)malloc(sizeof(t_philo) * round.n_philo);
+	i = 0;
 	while (i < round->n_philo)
 	{
-	// printf("initphilo terminate %d\n", round.terminate);
-	// printf("initphilo is_error %d\n", round.is_error);
 		(*philo)[i].id = i;
 		(*philo)[i].round = round;
-		// printf("init_philos %dth terminate %d\n", i, (*philo)[i].round->terminate);
-		// printf("init_philos %dth is_error %d\n", i, (*philo)[i].round->is_error);
-		// philo[i].cnt_eat = 0;
 		(*philo)[i].state = THINK;
-		// philo[i].fork1 = 0;
-		// philo[i].fork2 = 0;
 		i++;
 	}
 }
