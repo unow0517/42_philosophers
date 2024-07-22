141c8009799d4284b6552a0580126a15c2b93fce
Yun <unow1992@gmail.com>
2024-07-22 19:56:46 +0000

    norm all files

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: yowoo <yowoo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 19:41:27 by yowoo             #+#    #+#             */
-/*   Updated: 2024/07/22 19:46:19 by yowoo            ###   ########.fr       */
+/*   Updated: 2024/07/22 19:56:12 by yowoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,68 +21,68 @@
 
 typedef struct s_round
 {
-    int             n_philo;
-    int             t_die;
-    int             t_eat;
-    int             t_sleep;
-    int             n_must_eat; //OPTIONAL ARGUMENT, -1 if not given
-    struct timeval  begin;
-    int             terminate;
-    int             is_error;
-	pthread_mutex_t	print_m; //TO PROTECT PRINT_STR
-	pthread_mutex_t	*forks;
-	struct timeval	current;
-} t_round;
+	int					n_philo;
+	int					t_die;
+	int					t_eat;
+	int					t_sleep;
+	int					n_must_eat; //OPTIONAL ARGUMENT, -1 if not given
+	struct timeval		begin;
+	int					terminate;
+	int					is_error;
+	pthread_mutex_t		print_m; //TO PROTECT PRINT_STR
+	pthread_mutex_t		*forks;
+	struct timeval		current;
+}	t_round;
 
 enum e_state
 {
-    THINK,
-    EAT,
-    SLEEP
+	THINK,
+	EAT,
+	SLEEP
 };
 
 typedef struct s_philo
 {
-    int             id;
-    enum e_state    state;
-    struct timeval  last_eat;
-    pthread_t       thread;
-    t_round         *round;
-    int             cnt_eat;
-    int             fork1;
-    int             fork2;
-} t_philo;
+	int					id;
+	enum e_state		state;
+	struct timeval		last_eat;
+	pthread_t			thread;
+	t_round				*round;
+	int					cnt_eat;
+	int					fork1;
+	int					fork2;
+}	t_philo;
 
 //CHECK_INPUT.C
 //static        int	is_int(char *str)
-int             is_inpt_ok(int argc, char **argv);
-void            init_all(t_round *round, t_philo *philo, int argc, char **argv);
+int				is_inpt_ok(int argc, char **argv);
 
 //INITS.C
-void            init_struct(t_round *round, int argc, char **argv);
-void            init_philos(t_philo **philo, t_round *round);
+void			init_struct(t_round *round, int argc, char **argv);
+void			init_philos(t_philo **philo, t_round *round);
 
 //MAIN.C
-int main(int argc, char **argv);
+int				main(int argc, char **argv);
 
 //MONITOR.C
-void	*monitor_death_eat(void *philo);
+void			*monitor_death_eat(void *philo);
 
 //PRINTS.C
-void            print_str(char str, long stamp, t_philo *philo_s);
-void	        print_log(char str, t_philo *philo_s);
+void			print_str(char str, long stamp, t_philo *philo_s);
+void			print_log(char str, t_philo *philo_s);
 
 //THREAD_ROUTINES.C
 //static void   *loop_routine(t_philo *philo);
-void	        *thread_routine(void *philo);
+void			*thread_routine(void *philo);
 
-int             init_philo(t_philo *philo, int i);
+int				init_philo(t_philo *philo, int i);
 
 //UTILS.C
-int	            ft_atoi(const char *str);
-size_t	        get_current_time(void);
-int	            get_timestamp(struct timeval tv_in);
-int             clean(int end_mutex, t_philo *philo, t_round round, int end_threads);
-int             ft_usleep(size_t milliseconds);
+int				ft_atoi(const char *str);
+size_t			get_current_time(void);
+int				get_timestamp(struct timeval tv_in);
+int				clean(int end_mutex, t_philo *philo, \
+t_round round, int end_threads);
+int				ft_usleep(size_t milliseconds);
 
 #endif
--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: yowoo <yowoo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 20:30:38 by yowoo             #+#    #+#             */
-/*   Updated: 2024/07/15 20:08:03 by yowoo            ###   ########.fr       */
+/*   Updated: 2024/07/22 19:49:47 by yowoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,50 +48,38 @@ size_t	get_current_time(void)
 	return (time.tv_sec * 1000 + time.tv_usec / 1000);
 }
 
-//RETURN CURRENT TIME IN MILISECOND
 int	get_timestamp(struct timeval current)
 {
-	// printf("current time %d\n ", (int)get_current_time());
-	// printf("sec %ld, usec %ld\n ", current.tv_sec * 1000, current.tv_usec / 1000);
-	return ((int)(get_current_time() - (current.tv_sec * 1000 + current.tv_usec / 1000)));
+	return ((int)(get_current_time() - \
+	(current.tv_sec * 1000 + current.tv_usec / 1000)));
 }
 
-//DESTROY AND FREE MUTEX IN CASE OF ERROR
 int	clean(int end_mutex, t_philo *philo, t_round round, int end_threads)
 {
 	int	i;
 
-	//WHEN IS END_THREADS -1?
 	if (end_threads == -1)
 		end_threads = 0;
-	else //ANYWAY PRINT THIS OUT
+	else
 		printf("Error occured 2\n");
 	printf("Cleaning\n");
-	//JOIN RESULT OF I PHILO THREAD 
 	if (end_threads > 0)
 	{
-	printf("terminate to 1 in clean\n");
-
 		round.terminate = 1;
 		i = -1;
 		while (++i < end_threads)
 			pthread_join(philo[i].thread, NULL);
 	}
-	//IN WHICH CASE IS END MUTEX -2?
 	if (end_mutex == -2)
 		return (0);
 	i = -1;
-	//END MUTEX TO DESTROY MUTEX PROTECTING FORKS
 	while (++i < end_mutex)
 		pthread_mutex_destroy(&(round.forks[i]));
-	//DESTRORY PRINT_M MUTEX
 	pthread_mutex_destroy(&(round.print_m));
 	free(round.forks);
 	free(philo);
 	return (0);
 }
-
-//PTHREAD_CREATE WITH THREAD_ROUTINE ->IF ERROR, WIAT TILL THREAD ENDS WITH PTHREAD_JOIN IN CLEAN
 
 int	ft_usleep(size_t milliseconds)
 {
